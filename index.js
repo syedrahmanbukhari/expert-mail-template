c845afbcef8c8d4b28581ab29aa4f367f392dd35
syedrahmanbukhari92 <bukhari78615@gmail.com>
2026-02-03 12:30:04 +0500

    init commit

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Static files (images) ko serve karne ke liye
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 // "/" route par root ki html file serve
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "email-template.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+// Vercel ke liye export
+module.exports = app;
+
+// Local development ke liye
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
